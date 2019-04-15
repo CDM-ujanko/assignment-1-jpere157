a9da9a98363fc57e7df89595e0664f15652c7aea
Jeofrey Perez <jpere157@mail.depaul.edu>
2019-04-15 09:37:13 -0500

    first commit

---
+++ assgn_one.js
@@ -0,0 +1,16 @@
+
+function nameFun()
+{
+    var name;
+    var outputString;
+
+    name = document.getElementById('txtName').value;
+    outputString = 'Welcome to my very first page of IT238, ' + name;
+    
+    alert(outputString);
+}
+
+console.warn($);
+//EXTRA CREDIT: This warning indicates that jQuery is incorporated. The warn outputs a warning message to the console. 
+//I linked my assignment to a jQuery library found in the Google Hosted Libraries.
+console.warn(window);
